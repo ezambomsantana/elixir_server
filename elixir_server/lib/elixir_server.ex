9c20332d203962b2018832d31a52dd11ad8a80f4
ezambomsantana <ezambomsantana@gmail.com>
2017-12-22 20:12:55 -0200

    first server version

---
+++ elixir_server/lib/elixir_server.ex
@@ -0,0 +1,11 @@
+defmodule ElixirServer do
+  def init(default_opts) do
+    IO.puts "starting up Helloplug..."
+    default_opts
+  end
+
+  def call(conn, _opts) do
+    IO.puts "saying hello!"
+    Plug.Conn.send_resp(conn, 200, "Hello, world!")
+  end
+end
