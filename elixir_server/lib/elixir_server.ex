f7c8499a655f1ee5dbf510a89edf9d0b8e9eadd0
ezambomsantana <ezambomsantana@gmail.com>
2017-12-26 16:45:13 -0200

    params are working now

--- elixir_server/lib/elixir_server.ex
+++ elixir_server/lib/elixir_server.ex
@@ -1,8 +1,12 @@
 defmodule ElixirServer.Router do
+  import Plug.Conn
   use Plug.Router
 
   plug(:match)
   plug(:dispatch)
+
+  @carid   "id"
+  @value   "lat"
 
   def init(options) do
     Application.put_env(App, :pid_data, options, [])
@@ -12,15 +16,19 @@ defmodule ElixirServer.Router do
   get("/get", do: get(conn))
   match(_, do: send_resp(conn, 404, "Oops!\n"))
 
-  def save(conn) do
+  def save(conn) do    
+    conn = fetch_query_params(conn, []) # populates conn.params
+    %{ @carid => id, @value => lat } = conn.params
     pid = Application.get_env(App, :pid_data, nil) 
-    send(pid, {:save, 1, "1"})
+    send(pid, {:save, id, lat})
     send_resp(conn, 200, "save\n")
   end
 	
   def get(conn) do
+    conn = fetch_query_params(conn, []) # populates conn.params
+    %{ @carid => id } = conn.params
     pid = Application.get_env(App, :pid_data, nil) 
-    send(pid, {:get, 1, self()})
+    send(pid, {:get, id, self()})
     receive do
        {:value, value} -> send_resp(conn, 200, value)
     end
